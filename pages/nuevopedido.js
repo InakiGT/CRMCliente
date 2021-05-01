4514f6e67def1ad61b6ab5c2fd2c18724905d42f
Iñaki García <yarimfuego@gmail.com>
2021-05-01 15:21:52 -0500

    Agregado el routing para crear pedidos

---
+++ pages/nuevopedido.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react';
+import Layout from '../components/Layout';
+import Select from 'react-select';
+
+
+const NuevoPedido = () => {
+    return ( 
+        <Layout>
+            <h1 className="text-2xl text-gray-800 font-light">Crear Nuevo Pedido</h1>
+
+        </Layout>
+     );
+}
+ 
+export default NuevoPedido;
--- pages/pedidos.js
+++ pages/pedidos.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Layout from '../components/Layout';
+import Link from 'next/link';
 
 const Pedidos = () => {
     return ( 
         <Layout>
             <h1 className="text-3xl text-gray-800 font-light">Pedidos</h1>
+
+            <Link
+                href="/nuevopedido"
+            >
+            <a className="bg-blue-800 px-5 py-2 my-3 inline-block text-white rounded text-sm hover:bg-gray-800 uppercase font-bold">Nuevo Pedido</a>
+            </Link>
         </Layout>
      );
 }
