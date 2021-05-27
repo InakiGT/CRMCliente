5c16fc050992ed229e0c0b179b968711234d0288
Iñaki García <yarimfuego@gmail.com>
2021-05-26 19:04:36 -0500

    Agregado el componente de pedido

--- pages/pedidos.js
+++ pages/pedidos.js
@@ -1,8 +1,35 @@
 import React from 'react'
 import Layout from '../components/Layout';
 import Link from 'next/link';
+import {useQuery, gql} from '@apollo/client';
+
+import Pedido from '../components/Pedido';
+
+
+const OBTENER_PEDIDOS = gql`
+    query obtenerPedidosVendedor {
+        obtenerPedidosVendedor {
+            id
+            pedido {
+                id
+                cantidad
+            }
+            cliente
+            vendedor
+            total
+            estado
+        }
+    }
+`;
 
 const Pedidos = () => {
+
+    const {data, loading, error} = useQuery(OBTENER_PEDIDOS);
+
+    if(loading) return 'Cargando...';
+
+    const {obtenerPedidosVendedor} = data;
+
     return ( 
         <Layout>
             <h1 className="text-3xl text-gray-800 font-light">Pedidos</h1>
@@ -12,6 +39,17 @@ const Pedidos = () => {
             >
             <a className="bg-blue-800 px-5 py-2 my-3 inline-block text-white rounded text-sm hover:bg-gray-800 uppercase font-bold">Nuevo Pedido</a>
             </Link>
+
+            {obtenerPedidosVendedor.length === 0 ? (
+                <p className="mt-5 text-center text-2xl">No hay pedidos aún</p>
+            ) : (
+                obtenerPedidosVendedor.map(pedido => (
+                    <Pedido
+                        key={pedido.id}
+                        pedido={pedido}
+                    />
+                ))
+            )}
         </Layout>
      );
 }
